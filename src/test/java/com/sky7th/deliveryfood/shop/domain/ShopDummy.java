c7c4d6932d6f7e1d478217977fe7e5aefea68763
Taehwa Lee <xoghk0321@gmail.com>
2020-08-22 00:20:24 +0900

    feat 기존에 Shop 도메인 쪽에 있던 검증 로직을 Order 도메인으로 이동

--- src/test/java/com/sky7th/deliveryfood/shop/domain/ShopDummy.java
+++ src/test/java/com/sky7th/deliveryfood/shop/domain/ShopDummy.java
@@ -1,21 +1,12 @@
 package com.sky7th.deliveryfood.shop.domain;
 
 import com.sky7th.deliveryfood.generic.money.domain.Money;
-import com.sky7th.deliveryfood.shop.domain.Menu;
 import com.sky7th.deliveryfood.shop.domain.Menu.MenuBuilder;
-import com.sky7th.deliveryfood.shop.domain.MenuGroup;
-import com.sky7th.deliveryfood.shop.domain.MenuGroup.MenuGroupBuilder;
-import com.sky7th.deliveryfood.shop.domain.Shop;
-import com.sky7th.deliveryfood.shop.domain.Shop.ShopBuilder;
-import com.sky7th.deliveryfood.shop.domain.Option;
 import com.sky7th.deliveryfood.shop.domain.Option.OptionBuilder;
-import com.sky7th.deliveryfood.shop.domain.OptionGroup;
 import com.sky7th.deliveryfood.shop.domain.OptionGroup.OptionGroupBuilder;
-import com.sky7th.deliveryfood.shop.domain.OptionValidation;
-import com.sky7th.deliveryfood.shop.domain.OptionValidation.OptionValidationBuilder;
-import com.sky7th.deliveryfood.shop.domain.OptionGroupValidation;
 import com.sky7th.deliveryfood.shop.domain.OptionGroupValidation.OptionGroupValidationBuilder;
-
+import com.sky7th.deliveryfood.shop.domain.OptionValidation.OptionValidationBuilder;
+import com.sky7th.deliveryfood.shop.domain.Shop.ShopBuilder;
 import com.sky7th.deliveryfood.shop.domain.Shop.ShopStatus;
 import java.util.Arrays;
 
@@ -23,25 +14,25 @@ public class ShopDummy {
 
   public static ShopBuilder aShop() {
     return Shop.builder()
-        .name("shop 1")
+        .name("shop")
         .status(ShopStatus.ACTIVE)
         .minOrderAmount(Money.wons(20000));
   }
 
   public static MenuBuilder aMenu() {
     return Menu.builder()
-        .name("menu 1")
-        .description("menu 1 description")
+        .name("menu")
+        .description("menu description")
         .basic(OptionGroup.basic(
-            Option.builder().name("option 1-1").price(Money.wons(25000)).build(),
-            Option.builder().name("option 1-2").price(Money.wons(30000)).build()))
+            Option.builder().name("option 1-1").price(Money.wons(20000)).build(),
+            Option.builder().name("option 1-2").price(Money.wons(20000)).build()))
         .additives(Arrays.asList(
             OptionGroup.additive("option group 2", false,
                 Option.builder().name("option 2-1").price(Money.wons(0)).build(),
                 Option.builder().name("option 2-2").price(Money.wons(0)).build()),
             OptionGroup.additive("option group 3", true,
-                Option.builder().name("option group 3-1").price(Money.wons(500)).build(),
-                Option.builder().name("option group 3-2").price(Money.wons(2000)).build())
+                Option.builder().name("option group 3-1").price(Money.wons(20000)).build(),
+                Option.builder().name("option group 3-2").price(Money.wons(20000)).build())
         ));
   }
 
@@ -49,25 +40,25 @@ public class ShopDummy {
     return OptionGroup.builder()
         .selectable(true)
         .basic(true)
-        .name("option group 1")
+        .name("option group")
         .options(Arrays.asList(anOption().build()));
   }
 
   public static OptionBuilder anOption() {
     return Option.builder()
-        .name("option 1-1")
-        .price(Money.wons(0));
+        .name("option")
+        .price(Money.wons(20000));
   }
 
   public static OptionGroupValidationBuilder anOptionGroupValidation() {
     return OptionGroupValidation.builder()
-        .name("option group validation 1-2")
-        .options(Arrays.asList(anOption().build()));
+        .name("option group")
+        .optionValidations(Arrays.asList(anOptionValidation().build()));
   }
 
   public static OptionValidationBuilder anOptionValidation() {
     return OptionValidation.builder()
-        .name("option validation 1-2")
-        .price(Money.wons(0));
+        .name("option")
+        .price(Money.wons(20000));
   }
 }
