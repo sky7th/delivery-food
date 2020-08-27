b4b3b072254e5f38719c6756506217c1482dc74e
Taehwa Lee <xoghk0321@gmail.com>
2020-08-27 19:51:07 +0900

    feat: Member Entity

---
+++ src/main/java/com/sky7th/deliveryfood/user/member/Member.java
@@ -0,0 +1,23 @@
+package com.sky7th.deliveryfood.user.member;
+
+import com.sky7th.deliveryfood.user.User;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "MEMBERS", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "EMAIL")
+})
+public class Member extends User {
+
+  @Column(name = "USERNAME", nullable = false, unique = true)
+  private String username;
+
+  public Member() {
+    super();
+  }
+}
