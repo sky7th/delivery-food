7696b6d5f8d7ea11fd848f6e7e807d43d580c2b7
Taehwa Lee <xoghk0321@gmail.com>
2020-08-30 20:59:51 +0900

    style: 토큰 검증 에러 한글로 변경

--- src/main/java/com/sky7th/deliveryfood/security/JwtTokenValidator.java
+++ src/main/java/com/sky7th/deliveryfood/security/JwtTokenValidator.java
@@ -25,19 +25,19 @@ public class JwtTokenValidator {
       Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
 
     } catch (SignatureException ex) {
-      logger.error("Invalid JWT signature");
+      logger.error("JWT 서명 확인 불가");
       throw new InvalidTokenRequestException("JWT", authToken, "Incorrect signature");
 
     } catch (MalformedJwtException ex) {
-      logger.error("Invalid JWT token");
+      logger.error("잘못된 JWT 구성");
       throw new InvalidTokenRequestException("JWT", authToken, "Malformed jwt token");
 
     } catch (ExpiredJwtException ex) {
-      logger.error("Expired JWT token");
+      logger.error("JWT 유효기간 초과");
       throw new InvalidTokenRequestException("JWT", authToken, "Token expired. Refresh required");
 
     } catch (UnsupportedJwtException ex) {
-      logger.error("Unsupported JWT token");
+      logger.error("JWT 형식 불일치");
       throw new InvalidTokenRequestException("JWT", authToken, "Unsupported JWT token");
 
     } catch (IllegalArgumentException ex) {
