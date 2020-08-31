5cc6bd17f5e5c2c8fec2be3035f5e92795e4d37b
Taehwa Lee <xoghk0321@gmail.com>
2020-08-31 17:40:21 +0900

    feat: Owner, Rider UniqueConstraint email 제거

--- src/main/java/com/sky7th/deliveryfood/user/owner/domain/Owner.java
+++ src/main/java/com/sky7th/deliveryfood/user/owner/domain/Owner.java
@@ -4,14 +4,11 @@ import com.sky7th.deliveryfood.user.User;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import lombok.Getter;
 
 @Getter
 @Entity
-@Table(name = "OWNERS", uniqueConstraints = {
-    @UniqueConstraint(columnNames = "EMAIL")
-})
+@Table(name = "OWNERS")
 public class Owner extends User {
 
   @Column(name = "USERNAME", nullable = false)
