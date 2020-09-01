7d64bab320a82076e52469c8be300e6b029074c4
Taehwa Lee <xoghk0321@gmail.com>
2020-09-01 23:55:14 +0900

    feat: Owner Equals and Hashcode

--- src/main/java/com/sky7th/deliveryfood/user/owner/domain/Owner.java
+++ src/main/java/com/sky7th/deliveryfood/user/owner/domain/Owner.java
@@ -1,6 +1,7 @@
 package com.sky7th.deliveryfood.user.owner.domain;
 
 import com.sky7th.deliveryfood.user.User;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -21,5 +22,23 @@ public class Owner extends User {
   public Owner(String email, String password, String username) {
     super(email, password);
     this.username = username;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Owner owner = (Owner) o;
+    return Objects.equals(this.getId(), owner.getId()) &&
+        this.getRole() == owner.getRole();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.getId(), this.getRole());
   }
 }
